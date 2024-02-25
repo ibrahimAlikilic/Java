5135ba47c861f451b1f8128a2ce69e59561d9d2d
ibrahimAlikilic <ibrahim.alikilic@gmail.com>
2024-02-25 13:46:26 +0300

    Baslangic

---
+++ BTK Akademi Egitimler 1/Degiskenler/src/Degiskenler/Degiskenler.java
@@ -0,0 +1,14 @@
+package Degiskenler;
+
+public class Degiskenler {
+
+	public static void main(String[] args) {
+		// Java case sensetiv ( Büyük küçük harf duyarlı ) bir dildir
+		// reusability = tekrar tekrar kullanılabilirlik
+		int ogrenciSayisi=100;
+		String metin = "Ogrenci sayisi : ";
+		System.out.print(metin+ogrenciSayisi+"\n");
+		
+	}
+
+}
---
+++ BTK Akademi Egitimler 1/HelloWorld/src/HelloWorld/HelloWorld.java
@@ -0,0 +1,14 @@
+package HelloWorld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Merhaba Dünya"); 
+		System.out.println("Hello World"); 
+		// System.out Dışarıya çıktıv vericem demektir
+		// println sayesinde bana 1 satırlık alan oluştur demiş olduk ln olmazsa ve \n de kulanmazsan sonraki yazdığın alt satıra geçmez
+		
+	}
+
+}
